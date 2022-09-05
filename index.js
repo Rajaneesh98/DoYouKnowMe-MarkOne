45c21540c52faad5d92289aeb9d06c06fc03a313
Rajaneesh <65807307+Rajaneesh98@users.noreply.github.com>
2022-09-05 04:26:48 +0000

    scoring System

--- index.js
+++ index.js
@@ -1,51 +1,53 @@
 let readLineSync = require('readline-sync');
 
-let userName= readLineSync.question("What is you'r Name? ")
+let userName = readLineSync.question("What is you'r Name? ")
 
-console.log("Welcome ",userName)
+console.log("Welcome ", userName)
 console.log("Let's Start the Quiz!")
 console.log("-------------")
 
-let score =0;
+let score = 0;
 
-let one={
-  question:"Where do i live? ",
-  answer:"Hyderabad",
+let one = {
+  question: "Where do i live? ",
+  answer: "Hyderabad",
 }
-let two={
-  question:"Where do i work? ",
-  answer:"Capgemini",
+let two = {
+  question: "Where do i work? ",
+  answer: "Capgemini",
 }
-let three={
-  question:"What is my favorite Color? ",
-  answer:"White",
+let three = {
+  question: "What is my favorite Color? ",
+  answer: "White",
 }
-let four={
-  question:"Where is my Home Town? ",
-  answer:"Puttaparthi",
+let four = {
+  question: "Where is my Home Town? ",
+  answer: "Puttaparthi",
 }
-let five={
-  question:"What is my Age? ",
-  answer:"24",
+let five = {
+  question: "What is my Age? ",
+  answer: "24",
 }
 
-let quiz=[one,two,three,four,five]
+let quiz = [one, two, three, four, five]
 
-function play(question,answer){
-  let userAnswer=readLineSync.question(question)
-  if(userAnswer.toUpperCase()===answer.toUpperCase()){
+function play(question, answer) {
+  let userAnswer = readLineSync.question(question)
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) {
     console.log("You are Right! ")
-    score++
-  }else{
+    score += 2
+  } else {
     console.log("Wrong Answer! ")
     score--
   }
+  console.log("Your score is: ", score)
+  console.log("-------------")
 }
 
-for(let i=0 ; i<quiz.length ;i++){
-  que=quiz[i].question
-  ans=quiz[i].answer
-  play(que,ans)
+for (let i = 0; i < quiz.length; i++) {
+  que = quiz[i].question
+  ans = quiz[i].answer
+  play(que, ans)
 }
 
-console.log("The Final Score is ",score)
+console.log("The Final Score is ", score)
